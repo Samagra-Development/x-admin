bf3c48ae55e4055ff364892f4a78867350ce50ac
Shruti3004 <shruti@samagragovernance.in>
2021-09-24 09:41:46 +0530

    fix: removed the duplicate code
    Signed-off-by: Shruti3004 <shruti@samagragovernance.in>

---
+++ portal/components/react-admin/base/styles/index.js
@@ -0,0 +1,112 @@
+import { makeStyles } from "@material-ui/core";
+import blueGrey from "@material-ui/core/colors/blueGrey";
+
+export const useStyles = makeStyles((theme) => ({
+  searchBar: {
+    "& > div": {
+      fontSize: "1rem",
+    },
+  },
+  smSearchBar: {
+    "& > div": {
+      fontSize: "1.2rem",
+    },
+  },
+  smList: {
+    margin: "1rem 4rem",
+    "& > div": {
+      paddingLeft: 0,
+      backgroundColor: "unset",
+      "&:first-child > div": {
+        backgroundColor: "unset",
+      },
+      "&:last-child > div": {
+        backgroundColor: "#FFF",
+        boxShadow:
+          "0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)",
+      },
+    },
+  },
+  list: {
+    margin: "0rem 2rem",
+  },
+  filter: {
+    paddingLeft: 0,
+  },
+  grid: {
+    display: "grid",
+    width: "100%",
+    gridTemplateColumns: "1fr 1fr 1fr",
+    gridRowGap: "1ch",
+    gridColumnGap: "1ch",
+    margin: "1rem 0",
+    "& > td": theme.overrides.MuiTableCell.head,
+    "& > span": {
+      fontSize: "1.1rem",
+    },
+  },
+  fullWidthGrid: {
+    gridTemplateColumns: "1fr",
+    margin: "0 auto",
+  },
+  heading: {
+    fontSize: "1.4rem",
+    lineHeight: "0.5rem",
+    fontWeight: 700,
+    fontVariant: "all-small-caps",
+  },
+  select: {
+    width: "30vw",
+    alignSelf: "center",
+    "& > div > div": {
+      fontSize: "1.1rem",
+      transform: "translate(12px 21px)",
+    },
+  },
+  filterSelect: {
+    width: "15vw",
+    alignSelf: "center",
+    "& > label": {
+      opacity: "0.7",
+      fontSize: "1rem",
+    },
+    "& > div": {
+      transform: "translate(0 5px)",
+    },
+    " .MuiInputLabel-shrink": {
+      transform: "translate(12px, 7px) scale(0.75)",
+    },
+    "& > div > div": {
+      paddingTop: "15px",
+      paddingBottom: "5px",
+    },
+  },
+  textInput: {
+    "& > label": {
+      fontSize: "1.1rem",
+    },
+  },
+  selectInput: {
+    minWidth: "unset",
+    "& > label": {
+      fontSize: "1.1rem",
+    },
+    "& > div > div": {
+      maxHeight: "1.1rem",
+    },
+  },
+  warning: {
+    margin: "0",
+    padding: "0",
+    paddingBottom: "1rem",
+    textAlign: "center",
+    width: "100%",
+    fontStyle: "oblique",
+  },
+  fullWidth: {
+    width: "100%",
+  },
+  grey: {
+    color: blueGrey[300],
+  },
+}));
